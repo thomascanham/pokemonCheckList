1f645f1c985aed6ef5ae8bccf0c2643121c0f62e
Tom <web@tomcanham.co.uk>
2022-02-12 21:27:56 +0000

    added a page wrapper component that contains some starting base styling for the app.

---
+++ components/Page.js
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    @font-face {
+        font-family: 'radnika_next';
+        src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+        font-weight: normal;
+        font-style: normal;
+    }
+
+    html {
+        --primary-color: #302AE6;
+        --secondary-color: #536390;
+        --font-color: #424242;
+        --bg-color: #fff;
+        --heading-color: #292922;
+        box-sizing: border-box;
+        font-size: 16px;
+    }
+    *, *::before, *::after {
+        box-sizing: inherit;
+    }
+    body {
+        font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        padding: 0;
+        margin: 0;
+        font-size: 1rem;
+        line-height: 2;
+        background: var(--bg-color);
+    }
+    h1, h2, h3, h4, h5, h6 {
+        color: var(--heading-color);
+        font-weight: 400;
+        line-height: 1.3;
+    }
+    h1 {
+        margin-top: 0;
+        font-size: 3.052rem;
+    }
+
+    h2 {font-size: 2.441rem;}
+
+    h3 {font-size: 1.953rem;}
+
+    h4 {font-size: 1.563rem;}
+
+    h5 {font-size: 1.25rem;}
+    p {
+        color: var(--font-color);
+    }
+`;
+
+export default function Page({ children }) {
+  return (
+    <>
+      <GlobalStyles />
+      <main>{children}</main>
+    </>
+  );
+}
+
+Page.propTypes = {
+  children: PropTypes.any,
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import PropTypes from 'prop-types';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Page from '../components/Page';
 import '../styles/globals.css';
 
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
 }
 
 MyApp.getInitialProps = async function ({ Component, ctx }) {
